74dabc39cfbfe9aaad7354210d9f8b7f6be506d4
admin <admin@DESKTOP-R9K3P3L>
2024-07-17 11:29:03 +0900

    SSManager 2024-07-17

--- SSManager/src/co/yedam/vo/StudentVO.java
+++ SSManager/src/co/yedam/vo/StudentVO.java
@@ -2,6 +2,16 @@ package co.yedam.vo;
 
 import java.util.Date;
 
+import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+
+//@Getter
+//@Setter
+//@ToString
+@Data
+
 public class StudentVO {
 	//필드
 	private String stdNo; //std_no
@@ -14,51 +24,7 @@ public class StudentVO {
 	//생성자
 	
 	//메소드
-	public String getStdNo() {
-		return stdNo;
-	}
-	public void setStdNo(String stdNo) {
-		this.stdNo = stdNo;
-	}
-	public String getStdName() {
-		return stdName;
-	}
-	public void setStdName(String stdName) {
-		this.stdName = stdName;
-	}
-	public String getStdPhone() {
-		return stdPhone;
-	}
-	public void setStdPhone(String stdPhone) {
-		this.stdPhone = stdPhone;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getBirthDate() {
-		return birthDate;
-	}
-	public void setBirthDate(String birthDate) {
-		this.birthDate = birthDate;
-	}
-	public Date getCreationDate() {
-		return creationDate;
-	}
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
-	
-	@Override
-	public String toString() {
-		return "StudentVO [stdNo=" + stdNo + ", stdName=" + stdName + ", stdPhone=" + stdPhone + ", address=" + address
-				+ ", birthDate=" + birthDate + ", creationDate=" + creationDate + "]";
-	}
-	
 	public String briefShow() {
 		return stdNo + "   " + stdName + "      " + stdPhone;
 	}
 }
-
